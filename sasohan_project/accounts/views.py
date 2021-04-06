efe1157ef1dc3048c83592f0d8b73446201edd97
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-04-06 17:19:00 +0900

    Add design for login and signup pages

--- sasohan_project/accounts/views.py
+++ sasohan_project/accounts/views.py
@@ -7,30 +7,39 @@ def login(request):
 	if request.method == 'POST':
 		# 회원정보 찾기
 		user = auth.authenticate(
-			request, username = request.POST['account'], password = request.POST['password']
+			request, username = request.POST['username'], password = request.POST['password']
 		)
 
 		if user is not None:
 			auth.login(request, user)	# Login
 			return redirect('home')
 		else:
-			return render(request, 'login.html', {'error': 'incorrect'})
+			return render(request, 'login.html', {'error': '아이디 또는 비밀번호가 틀립니다.'})
 	else:
 		return render(request, 'login.html')
 
 def signup(request):
-	if request.method == 'POST':    # POST방식으로 요청되면
-		if request.POST['password1'] == request.POST['password2']:  # 입력된 pw 2개 같은지 확인
-			# 회원정보 추가
-			user = User.objects.create_user(
-				username=request.POST['username'],
-				password=request.POST['password1']
-			)
-			# 로그인 처리
-			auth.login(request, user)
-			# blog 페이지로 redirect
-			return redirect('home')
-	return render(request, 'signup.html')
+	# POST방식으로 요청되면 (회원가입 요청)
+	if request.method == 'POST':
+		# 아이디, 비밀번호가 빈칸일 경우
+		if request.POST['username'] == "" or request.POST['password1'] == "":
+			return render(request, 'signup.html', {'error': '아이디와 비밀번호를 입력해주세요.'})
+
+		# 비밀번호 두개가 다를 경우
+		if request.POST['password1'] != request.POST['password2']:
+			return render(request, 'signup.html', {'error': '비밀번호가 다릅니다.'})
+		
+		# 회원정보 추가
+		user = User.objects.create_user(
+			username=request.POST['username'],
+			password=request.POST['password1']
+		)
+		# 로그인 처리
+		auth.login(request, user)
+		# blog 페이지로 redirect
+		return redirect('home')
+	else:
+		return render(request, 'signup.html')
 
 def logout(request):
 	if request.method == 'POST':
