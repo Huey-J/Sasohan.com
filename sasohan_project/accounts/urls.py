84b61a18a4d6ee0ec8b62264213be767185c10bc
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-04-20 19:28:04 +0900

    Add naver login button and link naver login

--- sasohan_project/accounts/urls.py
+++ sasohan_project/accounts/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('login/', views.login, name="login"),
 	path('signup/', views.signup, name="signup"),
 	path('logout/', views.logout, name="logout"),
+
+	path('naver/', include('allauth.urls')),	# allAuth (네이버로그인)
 ]
