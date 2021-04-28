5b2ced043511cd0b908040263b2b9814a0a72eca
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-04-28 20:21:29 +0900

    change login and signup pages from accounts app to root

--- sasohan_project/sasohan/urls.py
+++ sasohan_project/sasohan/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.home, name="home"),
-    path('accounts/', include('accounts.urls')),
+
+    path('login/', views.login, name="login"),
+	path('signup/', views.signup, name="signup"),
+	path('logout/', views.logout, name="logout"),
+
+    path('accounts/', include('allauth.urls')),	# allAuth (네이버로그인)
 ]
