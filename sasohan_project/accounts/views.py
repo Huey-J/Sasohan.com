f19a987d74f772280a7ce433dc8d48ee68696cac
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-05-13 16:58:33 +0900

    Fix error with signup function

--- sasohan_project/accounts/views.py
+++ sasohan_project/accounts/views.py
@@ -1,12 +1,10 @@
 # from django.shortcuts import render
 from django.shortcuts import render, redirect
 
-from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth import get_user_model
 
-# from django.contrib.auth import get_user_model
-
-# User = get_user_model()
+User = get_user_model()
 
 def login(request):
 	if request.method == 'POST':
