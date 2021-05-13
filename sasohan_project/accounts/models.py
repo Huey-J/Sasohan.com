1f30ba79657fe630e2643a3e4303637e9c470324
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-05-13 20:30:46 +0900

    Modify User Model

--- sasohan_project/accounts/models.py
+++ sasohan_project/accounts/models.py
@@ -2,4 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=100, null=True)
+    first_name = None
+    last_name = None
+    email = None
+    
+    is_private = models.BooleanField(default = True)
+    phone = models.CharField(max_length = 30, default = "")
+    nickname = models.CharField(max_length = 60, unique = True, default = "")
--- sasohan_project/accounts/views.py
+++ sasohan_project/accounts/views.py
@@ -1,11 +1,10 @@
-# from django.shortcuts import render
 from django.shortcuts import render, redirect
-
 from django.contrib import auth
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+# 로그인 페이지
 def login(request):
 	if request.method == 'POST':
 		# 회원정보 찾기
@@ -25,13 +24,13 @@ def login(request):
 def signup(request):
 	# POST방식으로 요청되면 (회원가입 요청)
 	if request.method == 'POST':
-		# 아이디, 비밀번호가 빈칸일 경우
+		# 아이디 또는 비밀번호가 빈칸일 경우
 		if request.POST['username'] == "" or request.POST['password1'] == "":
 			return render(request, 'signup.html', {'error': '아이디와 비밀번호를 입력해주세요.'})
 
-		# 전화번호, 이메일이 빈칸일 경우
-		if request.POST['phone'] == "" or request.POST['email'] == "":
-			return render(request, 'signup.html', {'error': '핸드폰번호와 이메일을 입력해주세요.'})
+		# 전화번호가 빈칸일 경우
+		if request.POST['phone'] == "":
+			return render(request, 'signup.html', {'error': '핸드폰번호를 입력해주세요.'})
 
 		# 비밀번호 두개가 다를 경우
 		if request.POST['password1'] != request.POST['password2']:
@@ -41,7 +40,6 @@ def signup(request):
 		user = User.objects.create_user(
 			username=request.POST['username'],
 			password=request.POST['password1'],
-			email=request.POST['email'],
 			phone=request.POST['phone']
 		)
 		# 로그인 처리
