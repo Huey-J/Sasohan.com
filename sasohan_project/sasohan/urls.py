41a6a6fe4a665c65b440618ea07a072ce94a0997
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-06-24 16:14:31 +0900

    Add blog app and access management by page

--- sasohan_project/sasohan/urls.py
+++ sasohan_project/sasohan/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.home, name="home"),
     path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls'))
 ]
